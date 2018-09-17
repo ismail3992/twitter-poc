c90a87fea4a128e213a444a10fdd615875db4398
Ismail <ismail3992@users.noreply.github.com>
2018-09-17 14:10:19 +0530

    css changes

--- src/atoms/Navbar/index.js
+++ src/atoms/Navbar/index.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const NavBar = styled.div`
-background-color: #d6f0fb;
+background-color: #fff;
 position: fixed;
 top: 0;
 width: 100%;
@@ -10,19 +10,20 @@ padding: 0;
 display: block;
 color: #black;
 text-decoration: none;
+box-shadow: 0 0 1px #a9a9a9;
 `;
 
 
 export const NavBarItem = styled.a`
 float: left;
 display: block;
-color: #black;
+color: #66757f;
 text-align: center;
 padding: 14px 16px;
 text-decoration: none;
 font-size: 17px;
+cursor:pointer;
 :hover{
-  background: #4fa5eb;
-  color: black;
-
+  color: #1DA1F2;
+  border-bottom: 2px solid;
 }`;
--- src/molecules/tweet-list/tweet.js
+++ src/molecules/tweet-list/tweet.js
@@ -2,16 +2,17 @@ import React from 'react'
 
 const Tweet = (props) =>{
   const hashtags = props.entities.hashtags.map(hashtag => <div>{hashtag.text}</div>)
-  const urls = props.entities.urls.map(url => <div>{url.url}</div>)
+  const urls = props.entities.urls.map(url => <a href={url.url}> {url.url} </a>)
   var mediaArray = props.extended_entities
   let medias ='';
+  const name = props.user.name 
+  const screenName =  ' @'+props.user.screen_name
   if (mediaArray){
      medias = props.extended_entities.media.map(med => <img src= {med.media_url}/> )
   }
   return(<div className="tweet">
-  <div>{ props.id_str }</div>
-  <div>{ props.user.screen_name }</div>
-  <div>{ props.text }</div>
+  <h5>{ name } {screenName}</h5> 
+  <p>{ props.text }</p>
   <div>{hashtags }</div>
   <div>{medias}</div>
   <div>{urls}</div>
